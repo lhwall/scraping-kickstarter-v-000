edc1243f6ef297251e727d8a10e909b6ca6a159d
Lucy Wall <lhwall@gmail.com>
2018-04-09 13:27:52 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,5 +5,5 @@ require 'nokogiri'
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
-  kickstarer = Nokogiri::HTML(html)
+  kickstarter = Nokogiri::HTML(html)
 end
